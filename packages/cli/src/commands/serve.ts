a2e575e572bf1143e186f3388b57d0311957e3ed
Itamar Axelrad <iaxelrad@gmail.com>
2021-06-13 14:59:47 +0300

    Determining Our Execution Environment

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,8 @@
 import path from 'path';
-import { Command, description } from 'commander';
+import { Command } from 'commander';
 import { serve } from 'local-api';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
@@ -9,7 +11,12 @@ export const serveCommand = new Command()
   .action(async (filename = 'notebook.js', options: { port: string }) => {
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
-      await serve(parseInt(options.port), path.basename(filename), dir);
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
       console.log(
         `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`
       );
